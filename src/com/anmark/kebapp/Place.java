3416150015d1930ae0783f25c52e60300f0308e5
Anmar Khazal <anmarkhazal@gmail.com>
2013-08-24 12:55:47 +0200

    splashscreen, load places

---
+++ src/com/anmark/kebapp/Place.java
@@ -0,0 +1,132 @@
+package com.anmark.kebapp;
+
+import java.io.Serializable;
+
+import com.google.api.client.util.Key;
+
+/** Implement this class from "Serializable"
+* So that you can pass this class Object to another using Intents
+* Otherwise you can't pass to another actitivy
+* */
+public class Place implements Serializable {
+
+	@Key
+	public double rating;
+
+	@Key
+	public String id;
+	
+	@Key
+	public String name;
+	
+	@Key
+	public String reference;
+	
+	@Key
+	public String icon;
+	
+	@Key
+	public String vicinity;
+	
+	@Key
+	public Geometry geometry;
+	
+	@Key
+	public String formatted_address;
+	
+	@Key
+	public String formatted_phone_number;
+
+	@Override
+	public String toString() {
+		return name + " - " + id + " - " + reference;
+	}
+	
+	public static class Geometry implements Serializable
+	{
+		@Key
+		public Location location;
+	}
+	
+	public static class Location implements Serializable
+	{
+		@Key
+		public double lat;
+		
+		@Key
+		public double lng;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getVicinity() {
+		return vicinity;
+	}
+
+	public void setVicinity(String vicinity) {
+		this.vicinity = vicinity;
+	}
+
+	public Geometry getGeometry() {
+		return geometry;
+	}
+
+	public void setGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
+	public String getFormatted_address() {
+		return formatted_address;
+	}
+
+	public void setFormatted_address(String formatted_address) {
+		this.formatted_address = formatted_address;
+	}
+
+	public String getFormatted_phone_number() {
+		return formatted_phone_number;
+	}
+
+	public void setFormatted_phone_number(String formatted_phone_number) {
+		this.formatted_phone_number = formatted_phone_number;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+}
